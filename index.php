f32dd3629c5df3890c9fa575fa6b086403d2d129
Mathias Taylor <mtaylor53@mail.greenriver.edu>
2018-01-12 17:46:58 -0500

    Added additional roots

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
 
-    //turn on error reporting
-    ini_set('display_errors',1);
-    error_reporting(E_ALL);
+
+
     require_once ('vendor/autoload.php');
 
     $f3 = Base::instance();
 
+    //turn on error reporting
+    $f3->set('DEBUG',3);
+
     $f3->route('GET /', function() {
         echo '<h1>routing demo</h1>';
+    });
+
+    $f3->route('GET /page1', function(){
+        echo '<h1>This is page 1</h1>';
+    });
+
+    $f3->route('GET /page1/subpage-a', function(){
+        echo '<h1>This is page 2</h1>';
     });
 
     $f3->run();
